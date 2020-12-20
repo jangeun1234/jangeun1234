095e564018f7734ba7783375a48f907a5af927ef
jangeun1234 <je.lee@teamsparta.co>
2020-12-20 22:09:34 +0900

    Create week3_sweet.py

---
+++ week3_sweet.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# 아래 빈 칸('')을 채워보세요
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+# 아래 빈 칸('')을 채워보세요
+for song in songs:
+    title = song.select_one('#body-content > div.newest-list > div > table > tbody > tr').text
+
+    if title is not None:
+        rank = song.select_one('td.number')text[0:2].strip()
+        artist = song.select_one('td.info > a.artist ellipsis').text.strip()
+
+    print(rank, title, artist)
+
